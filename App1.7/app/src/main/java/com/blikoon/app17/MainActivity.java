79c1df6fec269ca95f0f65994c5d67dee0579723
Gakwaya <wayadn88@gmail.com>
2017-03-01 18:20:57 -0800

    Add App1.7

---
+++ App1.7/app/src/main/java/com/blikoon/app17/MainActivity.java
@@ -0,0 +1,60 @@
+/*
+        Showing text over Images in appropriate colors.
+        android.graphics.Bitmap and
+        android.support.v7.graphics.Palette classes are used.
+ */
+
+
+package com.blikoon.app17;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.support.v4.graphics.ColorUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.v7.graphics.Palette;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((ImageView) findViewById(R.id.first_photo)).setImageResource(R.drawable.first_photo);
+        Bitmap firstPhoto = BitmapFactory.decodeResource(getResources(), R.drawable.first_photo);
+        setTextColorForImage((TextView) findViewById(R.id.first_text), firstPhoto);
+
+        ((ImageView) findViewById(R.id.second_photo)).setImageResource(R.drawable.second_photo);
+        Bitmap secondPhoto = BitmapFactory.decodeResource(getResources(), R.drawable.second_photo);
+        setTextColorForImage(((TextView) findViewById(R.id.second_text)), secondPhoto);
+
+
+    }
+
+    private void setTextColorForImage(final TextView textView, Bitmap firstPhoto) {
+        Palette.from(firstPhoto)
+                .generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        Palette.Swatch swatch = palette.getVibrantSwatch();
+                        if (swatch == null && palette.getSwatches().size() > 0) {
+                            swatch = palette.getSwatches().get(0);
+                        }
+                        int titleTextColor = Color.WHITE;
+                        if (swatch != null) {
+                            titleTextColor = swatch.getTitleTextColor();
+                            titleTextColor = ColorUtils.setAlphaComponent(titleTextColor, 255);
+                        }
+                        textView.setTextColor(titleTextColor);
+                    }
+                });
+    }
+
+
+
+}
