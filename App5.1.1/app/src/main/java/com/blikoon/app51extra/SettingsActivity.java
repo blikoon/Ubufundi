724fc4921960cab524e30067adc258d80b5683b8
Gakwaya <wayadn88@gmail.com>
2017-04-13 11:59:48 +0800

    Add App5.1.1
    Work with the settings set by the user

---
+++ App5.1.1/app/src/main/java/com/blikoon/app51extra/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.blikoon.app51extra;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Load preference data from XML
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+}
